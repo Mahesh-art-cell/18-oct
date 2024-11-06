6bd160d61c2743f2bed8b75c2718d0fa84840ca0
Mahesh-art-cell <tallurim886@email.com>
2024-11-06 21:51:58 +0530

    all files are added

---
+++ js.js
@@ -0,0 +1,24 @@
+//1
+let str = "Hello World!";
+const res=str.replace("World","my Dear Friend")
+console.log(res);
+
+//2
+let a="Hungry";
+let rep=a.repeat(10)
+console.log(rep);
+
+//3
+let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever."
+let lastIndex=sentence.lastIndexOf("fox")
+console.log(lastIndex)
+
+//4
+let message = "Hello, world!";
+let start=message.startsWith("hi")
+console.log(start)
+
+//5
+let fileName = "document.pdf";
+let end=fileName.endsWith(".pdf")
+console.log(end);
